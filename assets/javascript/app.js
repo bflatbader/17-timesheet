e1a171fa30e746bc037de988f7d2cf60c9753e8e
digitalsamsquanch <digital.samsquanch@protonmail.com>
2019-09-17 20:14:59 -0400

    added the push to Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,22 +7,49 @@ var firebaseConfig = {
     storageBucket: "",
     messagingSenderId: "765354167485",
     appId: "1:765354167485:web:ba6a06aed221c8c3873bd4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  var empName = "";
-  var empRole = "";
-  var empStart;
-  var empRate;
+var empName = "";
+var empRole = "";
+var empStart;
+var empRate;
 
-  $("#addEmp").click(function(){
-      empName = $("#employee-name").val().trim();
-      empRole = $("#employee-role").val().trim();
-      empStart = $("#employee-start").val().trim();
-      empRate = $("#employee-rate").val().trim();
+$("#addEmp").click(function(){
+event.preventDefault();
+empName = $("#employee-name").val().trim();
+empRole = $("#employee-role").val().trim();
+empStart = $("#employee-start").val().trim();
+empRate = $("#employee-rate").val().trim();
 
-      console.log(empName, empRole, empStart, empRate);
-  })
+
+database.ref().push({
+    empName: empName,
+    empRole: empRole,
+    empStart: empStart,
+    empRate: empRate,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+});
+    console.log(empName, empRole, empStart, empRate);
+});
+
+database.ref().on("child_added", function(snapshot) {
+// storing the snapshot.val() in a variable for convenience
+var sv = snapshot.val();
+
+// Console.loging the last user's data
+console.log(sv.empName, sv.empRole, sv.empStart, sv.empRate);
+
+// Change the HTML to reflect
+$("#employee-name").text(sv.empName);
+$("#employee-role").text(sv.empRole);
+$("#employee-start").text(sv.empStart);
+$("#employee-name").text(sv.empRate);
+
+// Handle the errors
+}, function(errorObject) {
+console.log("Errors handled: " + errorObject.code);
+});
